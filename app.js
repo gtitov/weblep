606ca59c390dcd3577b21aaf3bb8b14708e15e95
gtitov <g.titov@marine-rc.ru>
2021-08-31 16:46:29 +0300

    Add satellite, remove other

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ yearLabel.innerText = yearRange.value
 const isoYear = yearRange.value + "-01-01"
 
 const map = L.map('mapid').setView([56.5, 37.09], 7);
-const OSM = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' });
-OSM.addTo(map)
+const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' });
+const satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ2hlcm1hbnQiLCJhIjoiY2pncDUwcnRmNDQ4ZjJ4czdjZXMzaHZpNyJ9.3rFyYRRtvLUngHm027HZ7A")
+
+// OSM.addTo(map)
+satellite.addTo(map)
 const getWMSLayer = function (layerId, time) {
     return (
         L.tileLayer.wms('https://powerlines.one/wms?', {
@@ -19,16 +22,22 @@ const getWMSLayer = function (layerId, time) {
 const pl = getWMSLayer("PL", isoYear).addTo(map)
 const substations = getWMSLayer("substations", isoYear).addTo(map)
 const generation = getWMSLayer("generation", isoYear).addTo(map)
-const other = getWMSLayer("other", isoYear).addTo(map)
+// const other = getWMSLayer("other", isoYear).addTo(map)
+
+
+const basemapControls = {
+    "Спутник": satellite,
+    "OSM": osm
+}
 
 const layersControls = {
     "ЛЭП": pl,
     "Подстанции": substations,
     "Станции": generation,
-    "Прочее": other,
+    // "Прочее": other,
 }
 
-L.control.layers({ OSM }, layersControls).addTo(map);
+L.control.layers(basemapControls, layersControls).addTo(map);
 
 
 const updateYearLabel = function () {
@@ -42,7 +51,7 @@ const updateYearValue = function () {
     pl.setParams({time: yearIso})
     substations.setParams({time: yearIso})
     generation.setParams({time: yearIso})
-    other.setParams({time: yearIso})
+    // other.setParams({time: yearIso})
 }
 
 
