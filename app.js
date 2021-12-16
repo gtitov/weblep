0f4c691740d3f551f861852befd85bbe9e312e3c
gtitov <g.titov@marine-rc.ru>
2021-12-16 11:51:54 +0300

    Update var names

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const satellite_label = "Satellite"
-const osm_label = "OSM"
-const pl_voltage_label = "Voltage of power lines"
-const pl_modifications_label = "Modifications of power lines"
-const endpoints_label = "Plants & substations"
+const satellite_legend = "Satellite"
+const osm_legend = "OSM"
+const pl_voltage_legend = "Voltage of power lines"
+const pl_modifications_legend = "Modifications of power lines"
+const endpoints_legend = "Plants & substations"
 const wms_attribution = "&copy A. M. Karpachevskiy, G. S. Titov"
 
 
@@ -13,7 +13,7 @@ const isoYear = yearRange.value + "-01-01"
 
 const map = L.map(
     'mapid',
-    {zoomControl: false}
+    { zoomControl: false }
 ).setView([56.5, 37.09], 7);
 
 L.DomEvent.on(L.DomUtil.get('zoom-in'), 'click', function () {
@@ -28,9 +28,10 @@ L.DomEvent.on(L.DomUtil.get('zoom-out'), 'click', function () {
 const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' });
 const satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaW93cTc1MCIsImEiOiJja3QwYzJpazMwN2ltMnVwOW0zbnJrOWh4In0.bBbrEq6D4MkMvX--IxJVUw")
 
+osm.addTo(map)
+// satellite.addTo(map)
 
-// OSM.addTo(map)
-satellite.addTo(map)
+
 const getWMSLayer = function (layerId, time) {
     return (
         L.tileLayer.wms('https://powerlines.one/wms?', {
@@ -42,23 +43,30 @@ const getWMSLayer = function (layerId, time) {
         })
     )
 }
+
 const pl_voltage = getWMSLayer("pl_voltage", isoYear).addTo(map)
 const pl_modifications = getWMSLayer("pl_modifications", isoYear)
 const endpoints = getWMSLayer("endpoints", isoYear).addTo(map)
 
 
 const basemapControls = {
-    [satellite_label]: satellite,
-    [osm_label]: osm
+    [satellite_legend]: satellite,
+    [osm_legend]: osm
 }
 
 const layersControls = {
-    [pl_voltage_label]: pl_voltage,
-    [pl_modifications_label]: pl_modifications,
-    [endpoints_label]: endpoints
+    [pl_voltage_legend]: pl_voltage,
+    [pl_modifications_legend]: pl_modifications,
+    [endpoints_legend]: endpoints
 }
 
 L.control.layers(basemapControls, layersControls).addTo(map);
+
+
+
+
+
+
 
 
 const updateYearLabel = function () {
@@ -69,9 +77,9 @@ const updateYearValue = function () {
     const yearValue = this.value
     this.setAttribute("value", yearValue)
     const yearIso = yearValue + "-01-01"
-    pl_voltage.setParams({time: yearIso})
-    pl_modifications.setParams({time: yearIso})
-    endpoints.setParams({time: yearIso})
+    pl_voltage.setParams({ time: yearIso })
+    pl_modifications.setParams({ time: yearIso })
+    endpoints.setParams({ time: yearIso })
     // other.setParams({time: yearIso})
 }
 
