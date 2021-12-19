65c6e0e9dc4dfa969ce9b77d092d354715aa214b
gtitov <g.titov@marine-rc.ru>
2021-12-19 11:03:26 +0300

    Add onclick functionality

--- app.js
+++ app.js
@@ -1,3 +1,74 @@
+L.TileLayer.BetterWMS = L.TileLayer.WMS.extend({
+
+    onAdd: function (map) {
+        // Triggered when the layer is added to a map.
+        //   Register a click listener, then do all the upstream WMS things
+        L.TileLayer.WMS.prototype.onAdd.call(this, map);
+        map.on('click', this.getFeatureInfo, this);
+    },
+
+    onRemove: function (map) {
+        // Triggered when the layer is removed from a map.
+        //   Unregister a click listener, then do all the upstream WMS things
+        L.TileLayer.WMS.prototype.onRemove.call(this, map);
+        map.off('click', this.getFeatureInfo, this);
+    },
+
+    getFeatureInfo: function (evt) {
+        const url = this.getFeatureInfoUrl(evt.latlng)
+        const showResults = L.Util.bind(this.showGetFeatureInfo, this)
+        fetch(url)
+            .then(response => response.json())
+            .then(data => showResults(evt.latlng, data))
+    },
+
+    getFeatureInfoUrl: function (latlng) {
+        // Construct a GetFeatureInfo request URL given a point
+        var point = this._map.latLngToContainerPoint(latlng, this._map.getZoom()),
+            size = this._map.getSize(),
+
+            params = {
+                request: 'GetFeatureInfo',
+                service: 'WMS',
+                srs: 'EPSG:4326',
+                styles: this.wmsParams.styles,
+                transparent: this.wmsParams.transparent,
+                version: this.wmsParams.version,
+                format: this.wmsParams.format,
+                bbox: this._map.getBounds().toBBoxString(),
+                height: size.y,
+                width: size.x,
+                layers: this.wmsParams.layers,
+                query_layers: this.wmsParams.layers,
+                info_format: 'application/json',
+                time: this.wmsParams.time
+            };
+
+        params[params.version === '1.3.0' ? 'i' : 'x'] = point.x;
+        params[params.version === '1.3.0' ? 'j' : 'y'] = point.y;
+
+        return this._url + L.Util.getParamString(params, this._url, true);
+    },
+
+    showGetFeatureInfo: function (latlng, response) {
+        console.log(response)
+        console.log(this.wmsParams)
+        // Otherwise show the content in a popup, or something.
+        if(!response.features.length) return
+        const content = `<p>${response.features[0].properties.Name_en}</p>`
+        L.popup({
+            maxWidth: 800
+        })
+            .setLatLng(latlng)
+            .setContent(content)
+            .openOn(this._map);
+    }
+});
+
+L.tileLayer.betterWms = function (url, options) {
+    return new L.TileLayer.BetterWMS(url, options);
+};
+
 const satellite_legend = "Satellite"
 const osm_legend = "OSM"
 const pl_voltage_legend = "Voltage of power lines"
@@ -34,7 +105,7 @@ satellite.addTo(map)
 
 const getWMSLayer = function (layerId, time) {
     return (
-        L.tileLayer.wms('https://powerlines.one/wms?', {
+        L.tileLayer.betterWms('https://powerlines.one/wms', {
             layers: layerId,
             transparent: true,
             format: 'image/png',
