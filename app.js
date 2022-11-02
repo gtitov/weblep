7e1c0457bde5374123aa747649d0e842d5cd98b6
gtitov <gherman.s.titov@gmail.com>
2022-11-02 15:58:37 +0300

    move to vector tiles

--- app.js
+++ app.js
@@ -1,149 +1,65 @@
-L.TileLayer.BetterWMS = L.TileLayer.WMS.extend({
+document.addEventListener("DOMContentLoaded", function() {
+    const vector_layers = {  // according to lep.json
+        "PL_voltage": "Voltage of power lines",
+        "PL_modifications": "Modifications of power lines",
+        "PL_age": "Age of power lines",
+        "Endpoints": "Plants & substations",
+        "El_centrality": "Centrality",
+        "PL_BC": "PL BC",
+        "EP_CC": "EP CC"
 
-    onAdd: function (map) {
-        // Triggered when the layer is added to a map.
-        //   Register a click listener, then do all the upstream WMS things
-        L.TileLayer.WMS.prototype.onAdd.call(this, map);
-        map.on('click', this.getFeatureInfo, this);
-    },
-
-    onRemove: function (map) {
-        // Triggered when the layer is removed from a map.
-        //   Unregister a click listener, then do all the upstream WMS things
-        L.TileLayer.WMS.prototype.onRemove.call(this, map);
-        map.off('click', this.getFeatureInfo, this);
-    },
-
-    getFeatureInfo: function (evt) {
-        const url = this.getFeatureInfoUrl(evt.latlng)
-        const showResults = L.Util.bind(this.showGetFeatureInfo, this)
-        fetch(url)
-            .then(response => response.json())
-            .then(data => showResults(evt.latlng, data))
-    },
-
-    getFeatureInfoUrl: function (latlng) {
-        // Construct a GetFeatureInfo request URL given a point
-        var point = this._map.latLngToContainerPoint(latlng, this._map.getZoom()),
-            size = this._map.getSize(),
-
-            params = {
-                request: 'GetFeatureInfo',
-                service: 'WMS',
-                srs: 'EPSG:4326',
-                styles: this.wmsParams.styles,
-                transparent: this.wmsParams.transparent,
-                version: this.wmsParams.version,
-                format: this.wmsParams.format,
-                bbox: this._map.getBounds().toBBoxString(),
-                height: size.y,
-                width: size.x,
-                layers: this.wmsParams.layers,
-                query_layers: this.wmsParams.layers,
-                info_format: 'application/json',
-                time: this.wmsParams.time
-            };
-
-        params[params.version === '1.3.0' ? 'i' : 'x'] = point.x;
-        params[params.version === '1.3.0' ? 'j' : 'y'] = point.y;
-
-        return this._url + L.Util.getParamString(params, this._url, true);
-    },
-
-    showGetFeatureInfo: function (latlng, response) {
-        console.log(response)
-        console.log(this.wmsParams)
-        // Otherwise show the content in a popup, or something.
-        if(!response.features.length) return
-        const content = `<p>${response.features[0].properties.Name_en}</p>`
-        L.popup({
-            maxWidth: 800
-        })
-            .setLatLng(latlng)
-            .setContent(content)
-            .openOn(this._map);
     }
-});
+    const satellite_legend = "Satellite"
+    const osm_legend = "OSM"
+    const pl_voltage_legend = "Voltage of power lines"
+    const pl_modifications_legend = "Modifications of power lines"
+    const pl_age_legend = "Age of power lines"
+    const endpoints_legend = "Plants & substations"
+    const el_centrality_legend = "el_centrality"
+    const pl_bc_legend = "pl_bc"
+    const ep_cc_legend = "ep_cc"
+    const wms_attribution = "&copy A. M. Karpachevskiy, G. S. Titov"
 
-L.tileLayer.betterWms = function (url, options) {
-    return new L.TileLayer.BetterWMS(url, options);
-};
+    const yearRange = document.getElementById("yearrange")
+    const yearLabel = document.getElementById("yearlabel")
+    yearLabel.innerText = yearRange.value
 
-const satellite_legend = "Satellite"
-const osm_legend = "OSM"
-const pl_voltage_legend = "Voltage of power lines"
-const pl_modifications_legend = "Modifications of power lines"
-const pl_age_legend = "Age of power lines"
-const endpoints_legend = "Plants & substations"
-const el_centrality_legend = "el_centrality"
-const pl_bc_legend = "pl_bc"
-const ep_cc_legend = "ep_cc"
-const wms_attribution = "&copy A. M. Karpachevskiy, G. S. Titov"
+    const map = new maplibregl.Map({
+        container: 'mapid', // container id
+        // DOCS: https://maplibre.org/maplibre-gl-js-docs/style-spec/
+        style: "lep.json",
+        center: [37.625, 55.751], // starting position [lng, lat]
+        zoom: 5, // starting zoom,
+        minZoom: 4,
+        maxZoom: 10
+    });
 
+    map.on("load", function() {
+        document.getElementById("zoom-in").addEventListener("click", () => map.zoomIn())
+        document.getElementById("zoom-out").addEventListener("click", () => map.zoomOut())
 
-const yearRange = document.getElementById("yearrange")
-const yearLabel = document.getElementById("yearlabel")
-yearLabel.innerText = yearRange.value
-const isoYear = yearRange.value + "-01-01"
+        // const a = () => {console.log(this.value)}
+        // yearRange.oninput = a
 
-const map = L.map(
-    'mapid',
-    { zoomControl: false }
-).setView([56.5, 37.09], 7);
+        const updateYearFilter = (year) => {
+            Object.keys(vector_layers).forEach(layer_name => {
+                map.setFilter(layer_name, ["==", ["get", "year"], year])
+            })
+        }
 
-L.DomEvent.on(L.DomUtil.get('zoom-in'), 'click', function () {
-    map.setZoom(map.getZoom() + 1);
-});
+        updateYearFilter(parseInt(yearRange.value))
 
-L.DomEvent.on(L.DomUtil.get('zoom-out'), 'click', function () {
-    map.setZoom(map.getZoom() - 1);
-});
-
-
-const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' });
-const satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaW93cTc1MCIsImEiOiJja3QwYzJpazMwN2ltMnVwOW0zbnJrOWh4In0.bBbrEq6D4MkMvX--IxJVUw")
-
-// osm.addTo(map)
-satellite.addTo(map)
-
-
-const getWMSLayer = function (layerId, time) {
-    return (
-        L.tileLayer.wms('https://powerlines.one/wms?', {  // change to "L.tileLayer.betterWms('https://powerlines.one/wms', {" to try betterWMS
-            layers: layerId,
-            transparent: true,
-            format: 'image/png',
-            time: time,
-            attribution: wms_attribution
+        yearRange.addEventListener("input", () => {
+            const yearValue = parseInt(yearRange.value)
+            yearLabel.innerText = yearValue
+            updateYearFilter(yearValue)
         })
-    )
-}
-
-const pl_voltage = getWMSLayer("pl_voltage", isoYear).addTo(map)
-const pl_modifications = getWMSLayer("pl_modifications", isoYear)
-const pl_age = getWMSLayer("pl_age", isoYear)
-const endpoints = getWMSLayer("endpoints", isoYear).addTo(map)
-const el_centrality = getWMSLayer("el_centrality", isoYear)
-const pl_bc = getWMSLayer("pl_bc", isoYear)
-const ep_cc = getWMSLayer("ep_cc", isoYear)
+    })
 
 
-const basemapControls = {
-    [satellite_legend]: satellite,
-    [osm_legend]: osm
-}
 
-const layersControls = {
-    [pl_voltage_legend]: pl_voltage,
-    [pl_modifications_legend]: pl_modifications,
-    [pl_age_legend]: pl_age,
-    [endpoints_legend]: endpoints,
-    [el_centrality_legend]: el_centrality,
-    [pl_bc_legend]: pl_bc,
-    [ep_cc_legend]: ep_cc
-}
 
-L.control.layers(basemapControls, layersControls).addTo(map);
+})
 
 
 
@@ -152,27 +68,49 @@ L.control.layers(basemapControls, layersControls).addTo(map);
 
 
 
-const updateYearLabel = function () {
-    yearLabel.innerText = this.value
-}
 
-const updateYearValue = function () {
-    const yearValue = this.value
-    this.setAttribute("value", yearValue)
-    const yearIso = yearValue + "-01-01"
-    pl_voltage.setParams({ time: yearIso })
-    pl_modifications.setParams({ time: yearIso })
-    pl_age.setParams({ time: yearIso })
-    endpoints.setParams({ time: yearIso })
-    el_centrality.setParams({ time: yearIso })
-    pl_bc.setParams({ time: yearIso })
-    ep_cc.setParams({ time: yearIso })
-    // other.setParams({time: yearIso})
-}
+// const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' });
+// const satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaW93cTc1MCIsImEiOiJja3QwYzJpazMwN2ltMnVwOW0zbnJrOWh4In0.bBbrEq6D4MkMvX--IxJVUw")
+
+// // osm.addTo(map)
+// satellite.addTo(map)
 
 
-yearRange.oninput = updateYearLabel
-yearRange.onchange = updateYearValue
+
+// const pl_voltage = getWMSLayer("pl_voltage", isoYear).addTo(map)
+// const pl_modifications = getWMSLayer("pl_modifications", isoYear)
+// const pl_age = getWMSLayer("pl_age", isoYear)
+// const endpoints = getWMSLayer("endpoints", isoYear).addTo(map)
+// const el_centrality = getWMSLayer("el_centrality", isoYear)
+// const pl_bc = getWMSLayer("pl_bc", isoYear)
+// const ep_cc = getWMSLayer("ep_cc", isoYear)
+
+
+// const basemapControls = {
+//     [satellite_legend]: satellite,
+//     [osm_legend]: osm
+// }
+
+// const layersControls = {
+//     [pl_voltage_legend]: pl_voltage,
+//     [pl_modifications_legend]: pl_modifications,
+//     [pl_age_legend]: pl_age,
+//     [endpoints_legend]: endpoints,
+//     [el_centrality_legend]: el_centrality,
+//     [pl_bc_legend]: pl_bc,
+//     [ep_cc_legend]: ep_cc
+// }
+
+// L.control.layers(basemapControls, layersControls).addTo(map);
+
+
+
+
+
+
+
+
+
 
 const legendToggle = document.getElementById("legend-toggle")
 const legend = document.getElementById("legend")
