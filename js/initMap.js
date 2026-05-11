0e02033db8787e81da56ab0fe4ef3f9508fa3b7b
gman <gman>
2026-05-11 13:32:35 +0300

    moscow map

--- js/initMap.js
+++ js/initMap.js
@@ -1,12 +1,14 @@
 maplibregl.addProtocol("pmtiles", new pmtiles.Protocol().tile)
+const MOSCOW = window.location.pathname === "/moscow.html/" || window.location.pathname === "/moscow.html"
 
 const map = new maplibregl.Map({
     style: 'https://raw.githubusercontent.com/gtitov/basemaps/refs/heads/master/positron-nolabels.json',
-    center: [90, 50],
-    zoom: 2.5,
+    center: MOSCOW ? [38.532, 55.477] : [90, 50],
+    zoom: MOSCOW ? 7 : 2.5,
     maxZoom: 12,
     container: 'map',
-    hash: true
+    hash: true,
+    maxBounds: MOSCOW ? [[28, 50], [48, 60]] : false
 })
 
 map.getCanvas().style.cursor = "crosshair"
