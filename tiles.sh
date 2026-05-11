65c4c436cb1d632c1fbe65352052774d064b1d0a
gman <gman>
2026-05-11 11:43:11 +0300

    moscow

---
+++ tiles.sh
@@ -0,0 +1,3 @@
+tippecanoe --output=pl7.pmtiles PL.geojson --maximum-zoom=7 --minimum-zoom=0 -l PL
+tippecanoe --output=points7.pmtiles Points.geojson --maximum-zoom=7 --minimum-zoom=0 -l points -r1
+tippecanoe --output=modifications7.pmtiles Modifications.geojson --maximum-zoom=7 --minimum-zoom=0 -l modifications
